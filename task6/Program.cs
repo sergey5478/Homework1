b13fe4b038b9d05b1d1795e9d515bf0296e57e70
sergey <sergey.fomin.90@inbox.ru>
2022-08-05 22:01:50 +0400

    add_task6

---
+++ task6/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int x;
+if (numberA > numberB)
+{
+    x = numberA;
+}
+else
+{
+    x = numberB;
+}
+if (numberC > x)
+{
+    x = numberC;
+}
+Console.WriteLine($"Max:{x}");
