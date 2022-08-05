39724752080281079211cbe18a7f0aeef5bf6ae5
sergey <sergey.fomin.90@inbox.ru>
2022-08-05 22:14:10 +0400

    add_task8

---
+++ task8/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("введите число N: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= numberA; i++)
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
