aa021f4b95de36c4e0126a2a3e93d1ddc344a810
sergey <sergey.fomin.90@inbox.ru>
2022-08-05 22:08:22 +0400

    add_task7

---
+++ task7/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if(numberA % 2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
