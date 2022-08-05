042e71d34ee142e4abe4b39c56d0ba4224a521e0
sergey <sergey.fomin.90@inbox.ru>
2022-08-05 21:52:27 +0400

    add_task5

--- task5/Program.cs
+++ task5/Program.cs
@@ -1,4 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-hgfkjh
-jhjhj
+﻿Console.WriteLine("введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if(numberA > numberB)
+{
+    Console.WriteLine($"Max:{numberA}");
+    Console.WriteLine($"Min:{numberB}");
+}
+else
+{
+    Console.WriteLine($"Max:{numberB}");
+    Console.WriteLine($"Min:{numberA}");
+}
